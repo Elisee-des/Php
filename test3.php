20cbc642f49baa114a8fe3daf671c17175c12c08
Elisee-des <esabidani@gmail.com>
2022-06-12 22:40:47 +0000

    test3 OK

--- test3.php
+++ test3.php
@@ -1,1 +1,21 @@
 <?php
+
+// $note = (int)readline("Entrez votre note:");
+
+// if ($note > 10) {
+//     echo "Bravo vous avez la moyenne\n";
+// }elseif ($note === 10)
+// {
+//     echo "Vous avez juste la moyenne peux mieux faire\n";
+// }else{
+//     echo "Desolé vous navez pas la moyenne\n";
+// }
+
+$heure = (int)readline("Entrez une heure:");
+
+if(($heure >= 8 && $heure <=12 ) || ($heure >= 14 && $heure <=17 )) 
+{
+    echo "Le magazin est ouvert";
+}else {
+    echo "Desoler le magasin est fermé a cette heure\n";
+}
